1319cfc32a690329f5f9b5eb1e806601ef75c6c7
coloradokim <schlesinger.kim@gmail.com>
2018-05-24 14:16:10 -0600

    initial commit

---
+++ app/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const responses = require('../responses')
+const cors = require('cors')
+const app = express()
+
+const findById = (responses, id) => {
+    return responses.find((response) => {
+        return response.id === id
+    })
+}
+
+app.use(cors())
+
+app.get('/', (req, res) => res.status(200).json(responses))
+
+app.get('/:id', (req, res) => {
+    let query = findById(responses.data, parseInt(req.params.id))
+    if (query === undefined) {
+        res.status(404).json({
+            error: {
+                message: "No record found!"
+            } 
+        })
+    } else {
+        res.status(200).json(query)
+    }
+    
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
---
+++ responses.js
@@ -0,0 +1,23 @@
+module.exports = {
+    data: [{
+        "id": 1,
+        "companyId": "31",
+        "questionId": "2",
+        "response": "no"
+    }, {
+        "id": 2,
+        "companyId": "90",
+        "questionId": "2",
+        "response": "yes"
+    }, {
+        "id": 3,
+        "companyId": "12",
+        "questionId": "2",
+        "response": "no"
+    }, {
+        "id": 4,
+        "companyId": "33",
+        "questionId": "2",
+        "response": "no"
+    }]
+}
